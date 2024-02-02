eae2bd826b0b3322e2c7437e2c317c149fda9d16
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2024-02-02 21:47:50 +0200

    commit2

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,19 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
-  }
+interface ComponentProps {
 }
 
-class Page extends Component {
-  pageInfo () {
+class Component<T extends ComponentProps> {
+  constructor(public props: T) {}
+}
+
+interface PageProps extends ComponentProps {
+  title: string;
+}
+
+class Page extends Component<PageProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,18 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<K, V> {
+  key: K;
+  value: V;
 }
 
+const numericKeyValuePair: KeyValuePair<number, string> = {
+  key: 42,
+  value: 'Hello, World!',
+};
+
+const stringKeyValuePair: KeyValuePair<string, number> = {
+  key: 'example',
+  value: 123,
+};
 export {};
